1d9c5c53cfc460404fb13647cb0d0a386edfbbf7
DoQuangHungDC21V7N548 <hungdc21v7n548@vlvh.ctu.edu.vn>
2025-10-01 16:40:17 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -8,5 +9,17 @@ app.use(express.json());
 
 const contactsRouter = require("./app/routes/contact.route");
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
